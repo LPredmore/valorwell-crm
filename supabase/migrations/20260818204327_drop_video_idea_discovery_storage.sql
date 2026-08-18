7dccc5a35ef56c06fae7e33c0ec41ce9d5237b92
LPredmore <124374222+LPredmore@users.noreply.github.com>
2026-08-18 15:43:37 -0500

    Drop retired video idea discovery storage

---
+++ supabase/migrations/20260818204327_drop_video_idea_discovery_storage.sql
@@ -0,0 +1,4 @@
+-- Feature-specific storage is no longer used after video-idea discovery retirement.
+drop table if exists public.ai_operations_viral_short_candidates;
+drop table if exists public.ai_operations_viral_short_runs;
+drop table if exists public.ai_operations_content_opportunities;
